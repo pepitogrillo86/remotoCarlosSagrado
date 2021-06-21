2f144105ef941c500e9f75b7425f1bdc8abf43d6
carlos <carlos@LAPTOP-MP192ADI>
2021-06-21 11:21:15 +0200

    Validacion en local

---
+++ proyectoCarlosSagrado/src/proyectoCarlosSagrado/Clase1.java
@@ -0,0 +1,14 @@
+package proyectoCarlosSagrado;
+
+public class Clase1 {
+
+	public Clase1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Soy la linea1 de la Clase1 creada por Carlos Sagrado");
+
+	}
+
+}
