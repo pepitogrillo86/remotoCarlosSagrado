1c8f3fc61894eba7bc25be246df9c7a689d16d85
carlos <carlos@LAPTOP-MP192ADI>
2021-06-21 11:32:50 +0200

    validacion en la rama en local

--- proyectoCarlosSagrado/src/proyectoCarlosSagrado/Clase1.java
+++ proyectoCarlosSagrado/src/proyectoCarlosSagrado/Clase1.java
@@ -8,7 +8,7 @@ public class Clase1 {
 
 	public static void main(String[] args) {
 		System.out.println("Soy la linea1 de la Clase1 creada por Carlos Sagrado");
-
+		System.out.println("Soy la linea2 de la Clase1 creada por Carlos Sagrado");
 	}
 
 }
---
+++ proyectoCarlosSagrado/src/proyectoCarlosSagrado/Clase2.java
@@ -0,0 +1,14 @@
+package proyectoCarlosSagrado;
+
+public class Clase2 {
+
+	public Clase2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Soy la linea 1 de la Clase2 y soy propiedad de Carlos Sagrado");
+
+	}
+
+}
